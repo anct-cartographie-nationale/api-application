b771df71b816c336e9555da78824fbfbb13cafc0
Marc Gavanier <marc.gavanier@gmail.com>
2022-06-14 17:11:03 +0200

    feat: add lieux mediation numerique schema

--- src/infrastructure/routes/lieux-mediation-numerique/get.route.ts
+++ src/infrastructure/routes/lieux-mediation-numerique/get.route.ts
@@ -23,10 +23,35 @@ import { LieuxMediationNumeriqueTransfer } from '../../transfers';
 export const handler = async (): Promise<APIGatewayProxyResultV2> => {
   const lieuxMediationNumerique: LieuxMediationNumeriqueTransfer[] = [
     {
-      id: '61e9260c2ac971550065e262',
-      latitude: 45.727292,
-      longitude: 4.88428,
-      nom: "Maison Lyon pour l'Emploi - Lyon 8"
+      id: '43493312300029',
+      nom: 'Anonymal',
+      commune: 'Reims',
+      code_postal: '51100',
+      code_insee: '51454',
+      adresse: '12 BIS RUE DE LECLERCQ',
+      complement_adresse: 'Le patio du bois de l’Aulne',
+      latitude: 43.52609,
+      longitude: 5.41423,
+      typologie: 'ACIPHC',
+      telephone: '01 80 05 98 80',
+      courriel: 'contact@laquincaillerie.tl',
+      site_web: 'https://www.laquincaillerie.tl/',
+      horaires: 'Mo-Fr 09:00-12:00,14:00-18:30; Sa 08:30-12:00',
+      presentation_resumee: 'Notre association propose des formations aux outils numériques à destination des personnes âgées.',
+      presentation_detail:
+        'Notre parcours d’initiation permet l’acquisition de compétences numériques de base. Nous proposons également un accompagnement à destination des personnes déjà initiées qui souhaiteraient approfondir leurs connaissances. Du matériel informatique est en libre accès pour nos adhérents tous les après-midis. En plus de d’accueillir les personnes dans notre lieu en semaine (sur rendez-vous), nous assurons une permanence le samedi matin dans la médiathèque XX.',
+      source: 'Hubik',
+      structure_parente: '130005481',
+      date_maj: '2022-06-02',
+      services:
+        'Gagner en autonomie dans les démarches administratives, Etre accompagné dans les démarches administratives, Prendre en main un smartphone ou une tablette, Prendre en main un ordinateur, Utiliser le numérique au quotidien, Approfondir ma culture numérique',
+      publics: 'Familles/enfants, Adultes, Déficience visuelle',
+      modalites_access: 'Payant, Gratuit',
+      labels_nationaux: 'France Services, APTIC, Point relais CAF',
+      labels_autres: 'SudLabs, Nièvre médiation numérique',
+      types_accompagnement: 'Seul, Avec de l’aide',
+      accessibilite: 'https://acceslibre.beta.gouv.fr/app/29-lampaul-plouarzel/a/bibliotheque-mediatheque/erp/mediatheque-13/',
+      prise_rdv: 'https://www.rdv-solidarites.fr/'
     }
   ];
 
