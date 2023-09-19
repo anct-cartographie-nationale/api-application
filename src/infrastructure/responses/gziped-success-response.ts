b9ea004eb11f062dc9bea1e68cf150733c7c4d73
Marc Gavanier <marc.gavanier@gmail.com>
2023-09-19 19:39:52 +0200

    feat: set cache control header to 24h (#27)

--- src/infrastructure/responses/gziped-success-response.ts
+++ src/infrastructure/responses/gziped-success-response.ts
@@ -5,7 +5,8 @@ export const gzipedSuccessResponse = <T>(body: T): APIGatewayProxyResultV2 => ({
   statusCode: 200,
   headers: {
     'Content-Type': 'application/json',
-    'Content-Encoding': 'gzip'
+    'Content-Encoding': 'gzip',
+    'Cache-Control': 'public, max-age=86400'
   },
   isBase64Encoded: true,
   body: gzipSync(JSON.stringify(body)).toString('base64')
