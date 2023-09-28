3d99767dfd8e07eec0a7ffe3f659c2ff1ac9a3f9
Marc Gavanier <marc.gavanier@gmail.com>
2023-09-28 14:22:32 +0200

    fix: success response headers (#37)

--- src/infrastructure/responses/success-response.ts
+++ src/infrastructure/responses/success-response.ts
@@ -3,9 +3,7 @@ import { APIGatewayProxyResultV2 } from 'aws-lambda';
 export const successResponse = <T>(body: T): APIGatewayProxyResultV2 => ({
   statusCode: 200,
   headers: {
-    'Content-Type': 'application/json',
-    'Cache-Control': 'public, max-age=86400'
+    'Content-Type': 'application/json'
   },
-  isBase64Encoded: true,
   body: JSON.stringify(body)
 });
