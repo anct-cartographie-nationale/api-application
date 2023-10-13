403178e255e4279287041c102b61da031c834e6f
Marc Gavanier <marc.gavanier@gmail.com>
2023-10-13 15:45:55 +0200

    feat: delete fingerprints without hash in patch fingerprint route (#57)

--- src/infrastructure/routes/lieux-inclusion-numerique/fingerprints/patch.route.ts
+++ src/infrastructure/routes/lieux-inclusion-numerique/fingerprints/patch.route.ts
@@ -1,18 +1,25 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, PutCommand, PutCommandOutput } from '@aws-sdk/lib-dynamodb';
+import { DeleteCommand, DynamoDBDocumentClient, PutCommand, PutCommandOutput } from '@aws-sdk/lib-dynamodb';
 import { findLieuxBySourceIndex } from '../../../dynamo-db';
 import { successResponse } from '../../../responses';
 import { LieuInclusionNumeriqueStorage } from '../../../storage';
-import { WriteFingerprintTransfer } from '../../../transfers';
+import { FingerprintTransfer } from '../../../transfers';
 
 /**
  * @openapi
- * /lieux-inclusion-numerique/fingerprints:
+ * /lieux-inclusion-numerique/fingerprints/{source}:
  *   patch:
  *     summary: Mettre à jour les empreintes numériques pour les lieux de médiation numérique dont la source et l'identifiant correspondent.
  *     description: Cette opération permet de mettre à jour les empreintes des lieux d'inclusion numérique en leur attribuant de nouvelles empreintes ou en modifiant les empreintes existantes pour chaque lieu correspondant aux références fournies.
  *     operationId: lieux-inclusion-numerique-fingerprints.patch
+ *     parameters:
+ *       - in: path
+ *         name: source
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Nom de la source de données à partir de laquelle récupérer la liste des empreintes numériques.
  *     security:
  *       - ApiKeyAuthorization: []
  *     requestBody:
@@ -25,13 +32,22 @@ import { WriteFingerprintTransfer } from '../../../transfers';
  *             items:
  *               $ref: '#/components/schemas/Fingerprint'
  *     responses:
- *       200:
- *         description: Les empreintes numériques à associer aux lieux ont étés traités avec succès.
  *       422:
  *         description: Le format des données fournies dans le body doit correspondre à un tableau empreintes numériques valide.
+ *       200:
+ *         description: Les empreintes numériques à associer aux lieux ou conduisant à leur suppression ont étés traités avec succès.
  */
 export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
-  const fingerprints: WriteFingerprintTransfer[] = JSON.parse(event.body ?? '[]');
+  const source: string | undefined = event.pathParameters?.['source'];
+
+  if (source == null) {
+    return {
+      statusCode: 422,
+      body: JSON.stringify({ message: 'Le paramètres "source" est obligatoire dans le chemin' })
+    };
+  }
+
+  const fingerprints: FingerprintTransfer[] = JSON.parse(event.body ?? '[]');
   const client: DynamoDBClient = new DynamoDBClient();
   const docClient: DynamoDBDocumentClient = DynamoDBDocumentClient.from(client, {
     marshallOptions: { convertClassInstanceToMap: true }
@@ -39,25 +55,35 @@ export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGateway
 
   try {
     await Promise.all(
-      fingerprints.map(async (fingerprint: WriteFingerprintTransfer): Promise<PutCommandOutput | undefined> => {
+      fingerprints.map(async (fingerprint: FingerprintTransfer): Promise<PutCommandOutput | undefined> => {
         const lieuInclusionNumeriqueFound: LieuInclusionNumeriqueStorage | undefined = await findLieuxBySourceIndex(docClient)(
-          fingerprint.source,
+          source,
           fingerprint.sourceId
         );
 
         if (lieuInclusionNumeriqueFound == null) return;
 
-        return docClient.send(
-          new PutCommand({
-            TableName: 'cartographie-nationale.lieux-inclusion-numerique',
-            Item: { ...lieuInclusionNumeriqueFound, hash: fingerprint.hash }
-          })
-        );
+        return fingerprint.hash == null
+          ? docClient.send(
+              new DeleteCommand({
+                TableName: 'cartographie-nationale.lieux-inclusion-numerique',
+                Key: {
+                  source: lieuInclusionNumeriqueFound.source,
+                  sourceId: lieuInclusionNumeriqueFound.sourceId
+                }
+              })
+            )
+          : docClient.send(
+              new PutCommand({
+                TableName: 'cartographie-nationale.lieux-inclusion-numerique',
+                Item: { ...lieuInclusionNumeriqueFound, hash: fingerprint.hash }
+              })
+            );
       })
     );
 
     return successResponse({
-      message: 'Les empreintes numériques à associer aux lieux ont étés traités avec succès.'
+      message: 'Les empreintes numériques à associer aux lieux ou conduisant à leur suppression ont étés traités avec succès.'
     });
   } catch {
     return {
--- src/infrastructure/transfers/fingerprint.transfer.ts
+++ src/infrastructure/transfers/fingerprint.transfer.ts
@@ -8,41 +8,29 @@ import { LieuInclusionNumeriqueStorage } from '../storage';
  *       required:
  *         - sourceId
  *         - source
- *         - hash
  *       type: object
  *       properties:
  *         sourceId:
  *           type: string
  *           description: Identifiant original dans la source.
  *           example: 42
- *         source:
- *           type: string
- *           description: Nom de la source associée à l'id.
- *           example: Hinaura
  *         hash:
  *           type: string
  *           description: Hash correspondant à la donnée originale associée à l'id dans la source originale.
  *           example: 39839892cbee68669c74545d3e868f04ecc22a19b0e8567410a47661c2464333
  */
-export interface WriteFingerprintTransfer {
+export interface FingerprintTransfer {
   sourceId: string;
-  source: string;
-  hash: string;
+  hash?: string;
 }
 
-export interface ReadFingerprintTransfer {
-  sourceId: string;
-  hash: string;
-}
+const onlyCompeteFingerprintTransfer = (fingerprint: Partial<FingerprintTransfer>): fingerprint is FingerprintTransfer =>
+  fingerprint.sourceId != null;
 
-const onlyCompeteFingerprintTransfer = (
-  fingerprint: Partial<ReadFingerprintTransfer>
-): fingerprint is ReadFingerprintTransfer => fingerprint.hash != null && fingerprint.sourceId != null;
-
-export const fromLieuxInclusionNumeriqueStorage = (lieux: LieuInclusionNumeriqueStorage[]): ReadFingerprintTransfer[] =>
+export const fromLieuxInclusionNumeriqueStorage = (lieux: LieuInclusionNumeriqueStorage[]): FingerprintTransfer[] =>
   lieux
     .map(
-      (lieu: LieuInclusionNumeriqueStorage): Partial<ReadFingerprintTransfer> => ({
+      (lieu: LieuInclusionNumeriqueStorage): Partial<FingerprintTransfer> => ({
         ...(lieu.hash ? { hash: lieu.hash } : {}),
         ...(lieu.sourceId ? { sourceId: lieu.sourceId } : {})
       })
