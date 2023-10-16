e74557e09667b0721ba7cb2bf9a30f6430fc6433
Marc Gavanier <marc.gavanier@gmail.com>
2023-10-16 12:20:45 +0200

    fix: remove pagination in all fingerprints by source index query (#59)

--- src/infrastructure/dynamo-db/source-index/all-fingerprints-by-source-index.ts
+++ src/infrastructure/dynamo-db/source-index/all-fingerprints-by-source-index.ts
@@ -1,18 +1,25 @@
-import { DynamoDBDocumentClient, QueryCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, QueryCommand, ScanCommandOutput } from '@aws-sdk/lib-dynamodb';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { LieuInclusionNumeriqueStorage } from '../../storage';
 
 export const allFingerprintsBySourceIndex =
   (docClient: DynamoDBDocumentClient = DynamoDBDocumentClient.from(new DynamoDBClient())) =>
-  async (source: string): Promise<LieuInclusionNumeriqueStorage[]> =>
-    (
-      await docClient.send(
-        new QueryCommand({
-          TableName: 'cartographie-nationale.lieux-inclusion-numerique',
-          IndexName: 'source-index',
-          ExpressionAttributeNames: { '#source': 'source' },
-          ExpressionAttributeValues: { ':source': source },
-          KeyConditionExpression: '#source = :source'
-        })
-      )
-    ).Items as LieuInclusionNumeriqueStorage[];
+  async (source: string): Promise<LieuInclusionNumeriqueStorage[]> => {
+    const allResponses: LieuInclusionNumeriqueStorage[] = [];
+    let ExclusiveStartKey: Record<string, LieuInclusionNumeriqueStorage> | undefined;
+
+    do {
+      const queryCommand: QueryCommand = new QueryCommand({
+        TableName: 'cartographie-nationale.lieux-inclusion-numerique',
+        IndexName: 'source-index',
+        ExpressionAttributeNames: { '#source': 'source' },
+        ExpressionAttributeValues: { ':source': source },
+        KeyConditionExpression: '#source = :source'
+      });
+
+      const dynamoDBResponse: ScanCommandOutput = await docClient.send(queryCommand);
+      allResponses.push(...((dynamoDBResponse.Items ?? []) as LieuInclusionNumeriqueStorage[]));
+      ExclusiveStartKey = dynamoDBResponse.LastEvaluatedKey;
+    } while (ExclusiveStartKey);
+    return allResponses;
+  };
