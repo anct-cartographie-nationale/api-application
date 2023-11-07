18735e8cdd19e63d5f73ac4c71bab531efcfa799
Marc Gavanier <marc.gavanier@gmail.com>
2023-11-07 17:56:21 +0100

    fix: exists value from query string is not a boolean

--- src/infrastructure/dynamo-db/filter/operators/attribute-exists.operator.ts
+++ src/infrastructure/dynamo-db/filter/operators/attribute-exists.operator.ts
@@ -10,5 +10,5 @@ export const attributeExistsOperator: QueryCommandOperator = <
   value: TValue
 ): QueryCommandExpression => ({
   ExpressionAttributeNames: { [`#${alias}`]: field },
-  FilterExpression: value ? `attribute_exists(#${alias})` : `attribute_not_exists(#${alias})`
+  FilterExpression: value === true || value === 'true' ? `attribute_exists(#${alias})` : `attribute_not_exists(#${alias})`
 });
