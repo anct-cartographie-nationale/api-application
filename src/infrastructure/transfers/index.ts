f9ee875f9b26f743234ae5768c5398a4bcde2a55
Marc Gavanier <marc.gavanier@gmail.com>
2023-11-09 15:17:52 +0100

    feat: add groups to delete to patch merge groups route (#73)

--- src/infrastructure/transfers/index.ts
+++ src/infrastructure/transfers/index.ts
@@ -1,6 +1,7 @@
 export * from './fingerprint.transfer';
 export * from './lieux-inclusion-numerique.transfer';
 export * from './merge-group.transfer';
+export * from './merge-groups-update.transfer';
 export * from './services-inclusion.transfer';
 export * from './source.transfer';
 export * from './structures-inclusion.transfer';
