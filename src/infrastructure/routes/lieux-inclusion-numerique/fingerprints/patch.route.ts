d67348d70e081a27599cf9a14cff4ec9fc7083ed
Marc Gavanier <marc.gavanier@gmail.com>
2023-11-13 20:28:07 +0100

    feat: remove merged lieu when it contains less that 2 lieux (#76)

--- src/infrastructure/routes/lieux-inclusion-numerique/fingerprints/patch.route.ts
+++ src/infrastructure/routes/lieux-inclusion-numerique/fingerprints/patch.route.ts
@@ -1,10 +1,57 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DeleteCommand, DynamoDBDocumentClient, PutCommand, PutCommandOutput } from '@aws-sdk/lib-dynamodb';
-import { findLieuxBySourceIndex } from '../../../dynamo-db';
+import { DeleteCommand, DynamoDBDocumentClient, UpdateCommand, UpdateCommandOutput } from '@aws-sdk/lib-dynamodb';
+import { scanAll } from '../../../dynamo-db';
 import { successResponse } from '../../../responses';
-import { LieuInclusionNumeriqueStorage } from '../../../storage';
+import { MergedLieuInclusionNumeriqueStorage } from '../../../storage';
 import { FingerprintTransfer } from '../../../transfers';
+
+const deleteLieu =
+  (docClient: DynamoDBDocumentClient) =>
+  async (id: string): Promise<void> => {
+    const mergedLieu: MergedLieuInclusionNumeriqueStorage | undefined = (
+      await scanAll<MergedLieuInclusionNumeriqueStorage>('cartographie-nationale.lieux-inclusion-numerique', {
+        ExpressionAttributeNames: { '#mergedIds': 'mergedIds' },
+        ExpressionAttributeValues: { ':id': id },
+        FilterExpression: 'contains(#mergedIds, :id)'
+      })
+    ).at(0);
+
+    if (mergedLieu != undefined && (mergedLieu.mergedIds ?? []).length < 2) {
+      await Promise.all(
+        mergedLieu.mergedIds.map(
+          async (lieuInGroupId: string): Promise<UpdateCommandOutput> =>
+            await docClient.send(
+              new UpdateCommand({
+                Key: { id: lieuInGroupId },
+                TableName: 'cartographie-nationale.lieux-inclusion-numerique',
+                ExpressionAttributeNames: { '#group': 'group' },
+                UpdateExpression: 'REMOVE #group'
+              })
+            )
+        )
+      );
+
+      await docClient.send(
+        new DeleteCommand({ TableName: 'cartographie-nationale.lieux-inclusion-numerique', Key: { id: mergedLieu.id } })
+      );
+    }
+
+    await docClient.send(new DeleteCommand({ TableName: 'cartographie-nationale.lieux-inclusion-numerique', Key: { id } }));
+  };
+
+const setHashToLieuMatching =
+  (docClient: DynamoDBDocumentClient) =>
+  async ({ sourceId: id, hash }: FingerprintTransfer): Promise<UpdateCommandOutput> =>
+    await docClient.send(
+      new UpdateCommand({
+        TableName: 'cartographie-nationale.lieux-inclusion-numerique',
+        Key: { id },
+        ExpressionAttributeNames: { '#hash': 'hash' },
+        ExpressionAttributeValues: { ':hash': hash },
+        UpdateExpression: 'SET #hash = :hash'
+      })
+    );
 
 /**
  * @openapi
@@ -55,28 +102,12 @@ export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGateway
 
   try {
     await Promise.all(
-      fingerprints.map(async (fingerprint: FingerprintTransfer): Promise<PutCommandOutput | undefined> => {
-        const lieuInclusionNumeriqueFound: LieuInclusionNumeriqueStorage | undefined = await findLieuxBySourceIndex(docClient)(
-          source,
-          fingerprint.sourceId
-        );
-
-        if (lieuInclusionNumeriqueFound == null) return;
-
-        return fingerprint.hash == null
-          ? docClient.send(
-              new DeleteCommand({
-                TableName: 'cartographie-nationale.lieux-inclusion-numerique',
-                Key: { id: lieuInclusionNumeriqueFound.id }
-              })
-            )
-          : docClient.send(
-              new PutCommand({
-                TableName: 'cartographie-nationale.lieux-inclusion-numerique',
-                Item: { ...lieuInclusionNumeriqueFound, hash: fingerprint.hash }
-              })
-            );
-      })
+      fingerprints.map(
+        async (fingerprint: FingerprintTransfer): Promise<void | UpdateCommandOutput> =>
+          fingerprint.hash == null
+            ? await deleteLieu(docClient)(fingerprint.sourceId)
+            : await setHashToLieuMatching(docClient)(fingerprint)
+      )
     );
 
     return successResponse({
