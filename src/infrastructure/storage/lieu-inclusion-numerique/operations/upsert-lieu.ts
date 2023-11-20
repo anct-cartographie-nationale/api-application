49105daf596eb1454d78813964f9dcf229424949
Marc Gavanier <marc.gavanier@gmail.com>
2023-11-20 23:34:26 +0100

    feat: use source and source id as id instead of uuid

--- src/infrastructure/storage/lieu-inclusion-numerique/operations/upsert-lieu.ts
+++ src/infrastructure/storage/lieu-inclusion-numerique/operations/upsert-lieu.ts
@@ -2,9 +2,9 @@ import { DynamoDBDocumentClient, PutCommand, PutCommandOutput } from '@aws-sdk/l
 import { LieuInclusionNumeriqueStorage } from '../lieu-inclusion-numerique.storage';
 import { reassignId } from '../../reassign-id/reassign-id';
 import { Id } from '@gouvfr-anct/lieux-de-mediation-numerique';
-import { v5 as uuid } from 'uuid';
 
-const UUID_NAMESPACE: string = '7dc3d274-abda-57fd-bd45-bf4adfeebcd3';
+export const idForLieu = (lieu: LieuInclusionNumeriqueStorage, lieuFound: LieuInclusionNumeriqueStorage | undefined): Id =>
+  lieuFound == undefined && lieu.mergedIds == null ? Id(`${lieu.source}@${lieu.id}`) : Id(lieu.id);
 
 export const upsertLieu =
   (docClient: DynamoDBDocumentClient) =>
@@ -15,13 +15,6 @@ export const upsertLieu =
     docClient.send(
       new PutCommand({
         TableName: 'cartographie-nationale.lieux-inclusion-numerique',
-        Item: reassignId(
-          lieuInclusionNumerique,
-          Id(
-            lieuInclusionNumeriqueFound == undefined
-              ? uuid(`${lieuInclusionNumerique.source ?? 'EMPTY_SOURCE'}:${lieuInclusionNumerique.id}`, UUID_NAMESPACE)
-              : lieuInclusionNumeriqueFound.id
-          )
-        )
+        Item: reassignId(lieuInclusionNumerique, idForLieu(lieuInclusionNumerique, lieuInclusionNumeriqueFound))
       })
     );
